0bc774dd125be03233fc5e5480cd72461d878339
DeepakGowda101 <deepak.devops.101@gmail.com>
2023-11-11 08:28:35 +0530

    onboard service

---
+++ src/main/java/com/uniqpay/onboardservice/ClientOnboard.java
@@ -0,0 +1,24 @@
+package com.uniqpay.onboardservice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController 
+@RequestMapping("/onboard")
+public class ClientOnboard {
+    @PostMapping()
+    public ResponseEntity<String> getOnboardClientsDetails(){
+        Map m1=new HashMap<>();
+        m1.put("name","Deepak");
+        m1.put("phoneNumber","9164226179");
+        return ResponseEntity.ok("client details found");
+    }
+}
---
+++ src/test/java/com/uniqpay/onboardservice/OnboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniqpay.onboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
